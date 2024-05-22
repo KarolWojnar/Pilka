eaa533b797294a1610bbcbc08551d37fdce53615
KarolWojnar <karolwojnar47@gmail.com>
2024-05-23 01:38:51 +0200

    clear code

--- football/src/main/java/com/Football/football/Controllers/FootballController.java
+++ football/src/main/java/com/Football/football/Controllers/FootballController.java
@@ -54,7 +54,7 @@ public class FootballController {
                     .method("GET", HttpRequest.BodyPublishers.noBody())
                     .build();
                 HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
-                model.addAttribute("fixtures", response);
+                model.addAttribute("fixtures", response.body());
         } catch (IOException | InterruptedException e) {
             model.addAttribute("error", "Błąd podczas przetawrzania danych: "
             + e.getMessage());
--- football/src/main/java/com/Football/football/Services/FixturesService.java
+++ football/src/main/java/com/Football/football/Services/FixturesService.java
@@ -1,18 +1,15 @@
 package com.Football.football.Services;
 
 import com.Football.football.Repositories.FixturesStatsRepo;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class FixturesService {
 
     private final FixturesStatsRepo fixturesRepository;
-
-    @Autowired
-    public FixturesService (FixturesStatsRepo fixturesRepository) {
-        this.fixturesRepository = fixturesRepository;
-    }
 
     public void getFixtures(int teamId) {
     }
