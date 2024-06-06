353000b4ee3f1efeb45bdb286ae6e2b1a786c225
KarolWojnar <karolwojnar47@gmail.com>
2024-06-07 00:13:17 +0200

    counting rating data refactored

--- football/src/main/java/com/Football/football/Repositories/PlayersStatsRepo.java
+++ football/src/main/java/com/Football/football/Repositories/PlayersStatsRepo.java
@@ -4,6 +4,7 @@ import com.Football.football.Tables.PlayerStats;
 import com.Football.football.Tables.TeamStats;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,11 +14,14 @@ public interface PlayersStatsRepo extends CrudRepository<PlayerStats, Long> {
     Optional<PlayerStats> getPlayerStatsByPlayerIdAndTeamStatsAndSeason(Long id, TeamStats teamId, Long season);
     Optional<PlayerStats> getPlayerStatsByPlayerIdAndSeason(Long id, Long season);
 
-    Iterable<PlayerStats> getStatystykiZawodnikasByPozycja(String pos);
+    Iterable<PlayerStats> getPlayerStatsByPozycja(String pos);
 
     @Query("select ps from PlayerStats ps group by ps.season, ps.teamStats")
     List<PlayerStats> getPlayerStatsGroupedBySeasonAndTeamStats();
 
     Iterable<PlayerStats> findPlayerStatsByTeamStats(TeamStats team);
     List<PlayerStats> findAllBySeason(long year);
+
+    @Query("select ps from PlayerStats ps where ps.pozycja in :positions")
+    Iterable<PlayerStats> findPlayerStatsByPozycjaIn(@Param("positions") List<String> positions);
 }
